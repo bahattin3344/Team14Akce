c31483241f19a721387ad0efff05213f878704ec
hp <busranuro.93@gmail.com>
2023-08-18 22:01:09 +0300

    Task2 bitti

--- src/Bushra/Bushra.java
+++ src/Bushra/Bushra.java
@@ -1,11 +1,13 @@
 package Bushra;
 
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Bushra {
 
     @Test
     public void Test1(){
-
+        WebDriver driver=new ChromeDriver();
     }
 }
