079c1567ecf70a21b027cae46a05ef2a68b629ae
deniz <denizan561@gmail.com>
2023-08-18 21:45:36 +0300

    Ekran Boyutu ve Element Bulma Hızı

--- src/MasterClass.java
+++ src/MasterClass.java
@@ -3,10 +3,15 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.time.Duration;
+
 public class MasterClass {
     @Test
     public void Master(){
         WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize(); // açılan ekranı tam ekran açmayı sağlıyor
+        driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(20)); // 30 saniyede chrome da sayfa açılmazsa kapat
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20)); // 30 saniye elementi bulma süresi verdik
         driver.get("https://www.akakce.com");
 
     }
