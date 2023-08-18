a73123c7824256ef786ea2f79366d394ed92d406
deniz <denizan561@gmail.com>
2023-08-18 21:18:25 +0300

    MyFuntion Kurulum

--- src/MasterClass.java
+++ src/MasterClass.java
@@ -1,10 +1,22 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class MasterClass {
     @Test
     public void Master(){
-
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://www.akakce.com");
 
     }
-}
+    public class MyFunction {
+        public static void wait(int sn) {
+            try {
+                Thread.sleep(sn*1000);
+            }
+            catch (InterruptedException e){
+                throw new RuntimeException(e);
+            }
+        }
+}}
