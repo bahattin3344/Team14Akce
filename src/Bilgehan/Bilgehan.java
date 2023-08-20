cbab04592c1f5b9811332ca4e34d717b599b51e8
bilgehanUnsal <bilgehanunsal@outlook.com.tr>
2023-08-20 14:32:06 +0300

    Projeye notlar eklendi, siparişler ve mesaj kutusundaki mesajlar karşılaştırıldı.

--- src/Bilgehan/Bilgehan.java
+++ src/Bilgehan/Bilgehan.java
@@ -1,5 +1,6 @@
 package Bilgehan;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -35,39 +36,49 @@ public class Bilgehan {
         MyFunc.wait(3);
 
         WebElement girisYapButton = driver.findElement(By.xpath("//input[@id='lfb']"));
-        Action action1 = aksiyonlar.moveToElement(girisYapButton).click().build();
+        Action action1 = aksiyonlar.moveToElement(girisYapButton).click().build(); // giriş yap butonuna tıkladı
         MyFunc.wait(2);
         action1.perform();
         MyFunc.wait(2);
 
         WebElement profil = driver.findElement(By.xpath("//a[@title='Hesabım']"));
-        Action action2 = aksiyonlar.moveToElement(profil).build();
+        Action action2 = aksiyonlar.moveToElement(profil).build(); // profil sekmesine gelip bekliyoruz HOVER
         MyFunc.wait(2);
         action2.perform();
 
         WebElement hesabim = driver.findElement(By.xpath("//a[text()='Hesabım']"));
-        Action action3 = aksiyonlar.moveToElement(hesabim).click().build();
+        Action action3 = aksiyonlar.moveToElement(hesabim).click().build(); // Profil sekmesinin altındaki hesabıma tıkladı
         MyFunc.wait(2);
         action3.perform();
 
         WebElement siparislerim = driver.findElement(By.xpath("//li[@class='order first']//a[text()='Siparişlerim']"));
-        Action action4 = aksiyonlar.moveToElement(siparislerim).click().build();
+        Action action4 = aksiyonlar.moveToElement(siparislerim).click().build(); // siparişlerime gelip tıkladı
         MyFunc.wait(2);
         action4.perform();
 
+        WebElement siparislerKontrolu = driver.findElement(By.xpath("//div[@class='no-record']"));
+        MyFunc.wait(2); // Siparişiniz bulunmuyor mesajı kontrol edildi. Hata varsa verilecek mesaj girildi.
+        Assert.assertTrue("Siparişler eşleşmedi...", siparislerKontrolu.getText().equals("Kayıtlı siparişiniz bulunmuyor."));
+        MyFunc.wait(2);
+
         WebElement profil2 = driver.findElement(By.xpath("//a[@title='Hesabım']"));
-        Action action5 = aksiyonlar.moveToElement(profil2).build();
+        Action action5 = aksiyonlar.moveToElement(profil2).build(); // profil sekmesine gelip bekliyoruz HOVER
         MyFunc.wait(2);
         action5.perform();
 
         WebElement hesabim2 = driver.findElement(By.xpath("//a[text()='Hesabım']"));
-        Action action6 = aksiyonlar.moveToElement(hesabim2).click().build();
+        Action action6 = aksiyonlar.moveToElement(hesabim2).click().build(); // Profil sekmesinin altındaki hesabıma tıkladı
         MyFunc.wait(2);
         action6.perform();
 
         WebElement mesajlarim = driver.findElement(By.xpath("//li[@class='order first']//a[text()='Mesajlarım']"));
-        Action action7 = aksiyonlar.moveToElement(mesajlarim).click().build();
+        Action action7 = aksiyonlar.moveToElement(mesajlarim).click().build(); // mesajlarıma gelip tıkladı
         MyFunc.wait(2);
         action7.perform();
+
+        WebElement mesajKontrolu = driver.findElement(By.xpath("//p[text()='Listelenecek mesaj bulunamadı.']"));
+        MyFunc.wait(2); // Mesaj kutusu kontrol edildi, hata varsa girilecek mesaj girildi.
+        Assert.assertTrue("Mesaj kontrolü eşleşmedi...", mesajKontrolu.getText().equals("Listelenecek mesaj bulunamadı."));
+        MyFunc.wait(2);
     }
 }
